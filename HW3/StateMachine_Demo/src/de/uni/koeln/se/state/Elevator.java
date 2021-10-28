09e89cbaeb0935d3c692ddff252a150e27f64e7b
Karim Dilling <ktk.dilling@freenet.de>
2021-10-28 15:39:18 +0200

    Finished elevator task

--- HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elevator.java
+++ HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elevator.java
@@ -11,14 +11,37 @@ public class Elevator{
 	public Elevator (int dest_floor)
 	{
 		this.dest_Floor=dest_floor;
-	System.out.println("*** Destination floor is: "+ dest_Floor);
-	arrive_atFloor();
+		System.out.println("*** Destination floor is: "+ dest_Floor);
+		arrive_atFloor(dest_floor);
 
 
 	}
 
-	private void arrive_atFloor() {
-		
+	private void arrive_atFloor(int dest_floor) {
+		if (current_Floor == dest_floor) {
+			System.out.println("Current State " + State.current_state);
+			System.out.println("*** Arrived at destination");
+		} else if (current_Floor < dest_floor) {
+			while(current_Floor < dest_floor) {
+				State.current_state = State.Moving_up;
+				System.out.println("*** Current state: " + State.current_state);
+				current_Floor++;
+				System.out.println("*** Current floor is " + current_Floor);
+			}
+			State.current_state = State.Idle;
+			System.out.println("*** Current state: " + State.current_state);
+			System.out.println("*** Arrived at destination");
+		} else {
+			while(current_Floor > dest_floor) {
+				State.current_state = State.Moving_down;
+				System.out.println("*** Current state: " + State.current_state);
+				current_Floor--;
+				System.out.println("*** Current floor is " + current_Floor);
+			}
+			State.current_state = State.Idle;
+			System.out.println("*** Current state: " + State.current_state);
+			System.out.println("*** Arrived at destination");
+		}
 	}
 
 	
--- HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elv_States.java
+++ HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elv_States.java
@@ -2,7 +2,13 @@ package de.uni.koeln.se.state;
 
 public class Elv_States {
 	
-	public  String Moving_up = "Moving_up";
+	public String Moving_up = "Moving_up";
+	public String Moving_down = "Moving_down";
+	public String Idle = "Idle";
+	public String current_state;
 
+	public Elv_States() {
+		current_state = Idle;
+	}
 
 }
