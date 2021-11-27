7b9c75f1e9e794d56bcf2d0b3d2c121bc5251dbc
Karim Dilling <64368956+karimdilling@users.noreply.github.com>
2021-11-27 16:11:20 +0100

    Update Client

--- HW7/HouseholdAppliance/src/de/uni/koeln/sse/se/Client.java
+++ HW7/HouseholdAppliance/src/de/uni/koeln/sse/se/Client.java
@@ -1,5 +1,3 @@
-package de.uni.koeln.sse.se;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,25 +7,28 @@ public class Client{
 public static void main(String[] args) {
 		
 
-		//Initialise some household
-		List<HouseholdItem> householdItmes= new ArrayList<>();
+	//Initialise some household
+	List<HouseholdItem> householdItems= new ArrayList<>();
 
-		
-		Electronic microwaveOven= new Electronic("Microwave Oven",37, 48, 27, 13, false);
-		Electronic tv= new Electronic("TV",122, 73, 15, 18, true);
-		
-		Glass wineGlass= new Glass("Wine glasses",33,28,23,1);
-		Glass coffeeTable= new Glass("Glass Coffee Table ",80,80,45,3);
 
-		
-		Furniture bed= new Furniture("Bed", "Metal", 205, 91, 94, 26);
-		Furniture cupboard= new Furniture("Bookshelf", "Wood", 60, 31, 190, 25);
+	Electronic microwaveOven= new Electronic("Microwave Oven",37, 48, 27, 13, false);
+	Electronic tv= new Electronic("TV",122, 73, 15, 18, true);
+
+	Glass wineGlass= new Glass("Wine glasses",33,28,23,1);
+	Glass coffeeTable= new Glass("Glass Coffee Table ",80,80,45,3);
+
+
+	Furniture bed= new Furniture("Bed", "Metal", 205, 91, 94, 26);
+	Furniture cupboard= new Furniture("Bookshelf", "Wood", 60, 31, 190, 25);
 		
 		
         List<HouseholdItem> namesList = Arrays.asList(microwaveOven,tv,wineGlass,coffeeTable,bed,cupboard );
-        householdItmes.addAll(namesList);
-		
-  
+        householdItems.addAll(namesList);
+
+	for (HouseholdItem householditem : householdItems) {
+		  System.out.println("The total cost for: " + householditem.getName() + " is " + householditem.getCost());
+		  householditem.printPackaging();
+	}
 }
 
 }
