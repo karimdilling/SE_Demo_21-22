8ee18e663cc6fa2a0d29b0ae9bfdca2a66b6291e
Karim Dilling <64368956+karimdilling@users.noreply.github.com>
2021-11-27 16:13:52 +0100

    Update HouseholdItem

--- HW7/HouseholdAppliance/src/de/uni/koeln/sse/se/HouseholdItem.java
+++ HW7/HouseholdAppliance/src/de/uni/koeln/sse/se/HouseholdItem.java
@@ -1,5 +1,22 @@
-package de.uni.koeln.sse.se;
-
 public interface HouseholdItem {
+    public int getHeight() ;
+
+    public void setHeight(int height) ;
+
+    public int getLength();
+
+    public void setLength(int length) ;
+
+    public int getWidth() ;
+
+    public void setWidth(int width) ;
+
+    public String getName() ;
+
+    public void setName(String name) ;
+
+    public double getCost();
+
+    public void printPackaging();
 
 }
