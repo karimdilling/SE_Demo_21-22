74c711a3450208284d940f2de0e6f841cbe12904
Karim Dilling <ktk.dilling@freenet.de>
2021-12-10 14:55:50 +0100

    Implemented Unit Tests for Calculator class

--- HW9/src/test/TestCalculator.java
+++ HW9/src/test/TestCalculator.java
@@ -1,5 +1,8 @@
 package test;
 
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -13,32 +16,32 @@ class TestCalculator {
 
 	@Test
 	public void Should_add_two_numbers_and_return_result() {
-		// Implement
+		assertEquals(4, calculator.add(3, 1));
 	}
 
 	@ParameterizedTest
-	@CsvSource({})
+	@CsvSource(value = {"4,3" , "5,2"})
 	public void Should_add_two_numbers_and_return_result_parameterized(int valueOne, int valueTwo) {
-		// Implement
+		assertEquals(7, calculator.add(valueOne, valueTwo));
 	}
 	
 	@Test
 	public void Should_substract_two_numbers_and_return_result() {
-		// Implement
+		assertEquals(5, calculator.sub(7, 2));
 	}
 	
 	@Test
 	public void Should_multiply_two_numbers_and_return_result() {
-		// Implement
+		assertEquals(9, calculator.multiply(3, 3));
 	}
 	
 	@Test
 	public void Should_divide_two_numbers_and_return_result() {
-		// Implement
+		assertEquals(3, calculator.divide(27, 9));
 	}
 	
 	@Test
 	public void Should_throw_an_arithmetic_exception_if_denominator_is_zero() {
-		// Implement
+		Throwable exception = assertThrows(ArithmeticException.class, () -> calculator.divide(0, 0) );
 	}
 }
