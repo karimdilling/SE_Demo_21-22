e667a644f466c639add34d0bf476f271e5c5efa8
Karim Dilling <ktk.dilling@freenet.de>
2021-12-10 17:01:16 +0100

    Updated calendar

--- HW9/src/main/Calendar.java
+++ HW9/src/main/Calendar.java
@@ -4,11 +4,15 @@ public class Calendar {
 
 	int year;
 
-	public Calendar(int year) {
-		this.year = year;
-	}
+    public Calendar(double year) {
+        if (year!=(int) year)
+            throw new IllegalArgumentException("YearMakesNoSense");
+        this.year = (int) year;
+    }
 
-	public boolean isLeapYear() {
-		return (this.year > 0 && this.year % 4 == 0) && (this.year % 100 != 0 || this.year % 400 == 0);
-	}
+    public boolean isLeapYear() {
+        if (year>=Integer.MAX_VALUE || year <=Integer.MIN_VALUE || year<0)
+            throw new IllegalArgumentException("Invalid range");
+        return (this.year > 0 && this.year % 4 == 0) && (this.year % 100 != 0 || this.year % 400 == 0);
+    }
 }
--- HW9/src/test/TestCalendar.java
+++ HW9/src/test/TestCalendar.java
@@ -1,27 +1,43 @@
 package test;
 
+import java.util.GregorianCalendar;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import main.Calendar;
 
 class TestCalendar {
 
 	@ParameterizedTest
-	@ValueSource()
+	@ValueSource(ints = {2004, 2008, 2012, 2016, 2020})
 	public void Should_return_true(int year) {
-		// Implement
+		Calendar cal = new Calendar(year);
+		assertTrue(cal.isLeapYear());
 	}
 
 	@ParameterizedTest
-	@ValueSource()
+	@ValueSource(ints = {1, 2, 3, 6, 7})
 	public void Should_return_false(int year) {
-		// Implement
+		Calendar cal = new Calendar(year);
+		assertFalse(cal.isLeapYear());
 	}
 
 	@ParameterizedTest
-	@ValueSource()
+	@ValueSource(ints = {2004, 2008, 2012, 2016, 2020})
 	public void Should_return_if_year_is_leap(int year) {
-		// Implement
+		GregorianCalendar gregCal = new GregorianCalendar();
+		assertTrue(gregCal.isLeapYear(year));
 	}
-
-	// Create a new method for boundary testing
+	
+	@Test
+    public void boundary_test() {
+        Calendar cal1 = new Calendar(Integer.MAX_VALUE+1);
+        Throwable e = assertThrows(IllegalArgumentException.class, () -> cal1.isLeapYear() );
+        Throwable e2 = assertThrows(IllegalArgumentException.class, () -> new Calendar(23.7) );
+    }
 }
